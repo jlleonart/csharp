fa133fe304b7fd23f25858048300293c9860ca7b
Joel Lleonart <jplleonart@gmail.com>
2021-09-25 11:13:49 -0400

    Done

---
+++ ifnestedsolution/ifnested/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ifnested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Enter an integer : ");
+            var ans = Console.ReadLine();
+            var nbr = Convert.ToInt32(ans);
+            if (nbr % 2 == 0)
+            { Console.WriteLine($"Number {nbr} is even"); }
+            else
+            { Console.WriteLine($"Number {nbr} is odd"); }
+        }
+    }
+}
